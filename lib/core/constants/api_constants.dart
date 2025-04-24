12213c9ca13b65327bff5a2d2fae44160832befb
Reza Sharifi <reza.sharifi76@gmail.com>
2025-04-24 23:57:52 +0330

    add Constant classes

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,15 @@
+class ApiConstants {
+  static const String authBaseUrl =
+      'https://x8ki-letl-twmt.n7.xano.io/api:0uinMmtC';
+  static const String coinBaseUrl =
+      'https://x8ki-letl-twmt.n7.xano.io/api:awZizgY2';
+
+  static const String login = '/auth/login';
+  static const String me = '/auth/me';
+  static const String signup = '/auth/signup';
+
+  static const String user = '/user';
+
+  static const String coins = '/cryptocurrency';
+  static const String favorites = '/favorites';
+}
---
+++ lib/core/constants/storage_key_constants.dart
@@ -0,0 +1,4 @@
+class StorageKeyConstants {
+  static const String authToken = 'authToken';
+  static const String appTheme = 'appTheme';
+}
